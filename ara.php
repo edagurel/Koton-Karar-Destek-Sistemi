7d5e960dd665edf44ca215d02ac40ecaf60787d2
edagurel <147712731+edagurel@users.noreply.github.com>
2025-01-25 02:06:58 +0300

    Add files via upload

---
+++ ara.php
@@ -0,0 +1,22 @@
+<?php
+$baglanti=mysqli_connect("localhost","root","root","kotonkds");
+if($_POST){
+if(strip_tags(trim(isset($_POST["kelime"])))){
+$kelime=$_POST["kelime"];
+}
+$sorgu=mysqli_query($baglanti,"SELECT * FROM subeler WHERE sube_adi='".$kelime."'");
+mysqli_query("SET NAMES 'utf8'");
+mysqli_query("SET CHARACTER SET utf8_general_ci");
+if(mysqli_num_rows($sorgu)>0){
+$row=mysqli_fetch_assoc($sorgu);
+}
+mysqli_close($baglanti);
+}else{
+echo "Veriler Gelmedi";
+}
+
+}else {
+die("Bağlantı Sağlanamadı");
+};
+
+?>
